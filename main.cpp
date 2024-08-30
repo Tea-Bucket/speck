83aa8dd99f06d50673c2241dc551f01efb56d91b
Max <max_sander@outlook.de>
2024-08-30 21:02:47 +0200

    speckage assembled

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "speck.hpp"
 int main(int argc, char *argv[]){
 	printf("Hello Speck!");
 }
--- speck.hpp
+++ speck.hpp
@@ -1,9 +1,41 @@
+
+#include <cstdint>
+#include <fstream>
+#include <map>
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 namespace speck{
 	struct speckage{
-	}
+		std::unordered_map<std::string, std::pair<uint64_t, uint64_t>> file_info;
+		void* data;
+		uint64_t data_size;
+	};
+
 
 	bool addFileToPackage(speckage& speckage, char* filepath);
 	bool savePackageToFile(const speckage& toSave, char* filepath);
-	speckage readPackageFromFile(char* filepath);
-	void* readFileFromPackage(const speckage& speckage, char* filepath, uint32_t& size);
+	speckage readPackageFromFile(std::string filepath)
+	{
+		std::ifstream file (filepath);
+		uint64_t header_size;
+		file >> header_size;
+		while (true)
+		{
+
+		}
+		return speckage();
+	};
+
+	///
+	/// @param speckage speckage to read from
+	/// @param filepath relative path inside speckage
+	/// @param size is output
+	/// @return pointer to beginning of output
+	char* readFileFromPackage(const speckage& speckage, std::string filepath, uint32_t& size)
+	{
+
+	};
 }
